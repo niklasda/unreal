e35f2b7b626ac3687f8c414610302829a94c67b2
Niklas <nd@sql8r.net>
2020-05-20 17:57:25 +0200

    rand

--- TripleX/TripleX/TripleX.cpp
+++ TripleX/TripleX/TripleX.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <ctime>
 
 
 void PrintIntroduction(int Difficulty)
@@ -16,10 +17,15 @@ void PrintIntroduction(int Difficulty)
 bool PlayGame(int Difficulty)
 {
 	PrintIntroduction(Difficulty);
-	
-	const int CodeA = 4, CodeB = 3, CodeC = 2;
-	int CodeSum = CodeA + CodeB + CodeC;
-	int CodeProduct = CodeA * CodeB * CodeC;
+
+
+	rand() % 4;
+
+	const int CodeA = rand() % Difficulty + Difficulty;
+	const int CodeB = rand() % Difficulty + Difficulty;
+	const int CodeC = rand() % Difficulty + Difficulty;
+	const int CodeSum = CodeA + CodeB + CodeC;
+	const int CodeProduct = CodeA * CodeB * CodeC;
 
 	// exiting stuff
 
@@ -27,38 +33,42 @@ bool PlayGame(int Difficulty)
 	not hard
 	*/
 
-	std::cout << "+ There are 3 numbers in the code " ;
-	std::cout << "\n+ The codes add-up to " << CodeSum ;
+	std::cout << "+ There are 3 numbers in the code ";
+	std::cout << "\n+ The codes add-up to " << CodeSum;
 	std::cout << "\n+ The codes multiply to give " << CodeProduct << std::endl;
 
 	int GuessA, GuessB, GuessC;
 
 	std::cin >> GuessA >> GuessB >> GuessC;
-	
+
 	// std::cout << "You entered: " << GuessA << GuessB << GuessC << std::endl;
 
 	int GuessSum = GuessA + GuessB + GuessC;
 	int GuessProduct = GuessA * GuessB * GuessC;
 
-	if(CodeSum == GuessSum && CodeProduct == GuessProduct)
+	if (CodeSum == GuessSum && CodeProduct == GuessProduct)
 	{
 		std::cout << "The code is correct, proceed!\n";
 		return true;
 	}
 	else
 	{
-		std::cout << "The code is wrong, you die!\n";
+		std::cout << "The code is wrong, retry!\n";
 		return false;
 	}
 }
 
 int main()
 {
+	srand(time(nullptr));
 
 	int LevelDifficulty = 1;
-	while (LevelDifficulty < 5)
+	const int MaxDifficulty = 5;
+
+
+	while (LevelDifficulty <= MaxDifficulty)
 	{
-		bool bLevelComplete = PlayGame(LevelDifficulty) ;
+		const bool bLevelComplete = PlayGame(LevelDifficulty);
 
 		std::cin.clear();
 		std::cin.ignore();
@@ -68,6 +78,8 @@ int main()
 			++LevelDifficulty;
 		}
 	}
-	
+
+	std::cout << "You mastered the last lock!\n";
+
 	return 0;
 }
