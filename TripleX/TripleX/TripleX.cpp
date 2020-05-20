0ad4c4a08a5ad3ea9dd4a89fc4924b8de505c90c
Niklas <nd@sql8r.net>
2020-05-20 16:00:37 +0200

    const

--- TripleX/TripleX/TripleX.cpp
+++ TripleX/TripleX/TripleX.cpp
@@ -10,12 +10,13 @@ int main()
 	std::cout << "Enter the correct code to continue..."; 
 	std::cout << std::endl;
 		
-	int a = 4;
-	int b = 3;
-	int c = 2;
+	const int a = 4;
+    const auto b = 3;
+    const int c = 2;
 
 	int sum = a + b + c;
 	int product = a * b *c;
+	
 	
 
 	std::cout << sum << std::endl;
