87940d63fb873c8ed313244812f2fd36cf2ece0d
Niklas <nd@sql8r.net>
2020-05-20 15:54:25 +0200

    sum/prod

--- TripleX/TripleX/TripleX.cpp
+++ TripleX/TripleX/TripleX.cpp
@@ -5,18 +5,21 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "You are a secret agent trying to break into a secure server room..." ;
+	std::cout << std::endl;
+	std::cout << "Enter the correct code to continue..."; 
+	std::cout << std::endl;
+		
+	int a = 4;
+	int b = 3;
+	int c = 2;
+
+	int sum = a + b + c;
+	int product = a * b *c;
+	
+
+	std::cout << sum << std::endl;
+	std::cout << product << std::endl;
 
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
