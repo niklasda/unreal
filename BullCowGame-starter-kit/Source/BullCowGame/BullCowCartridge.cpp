bef86a96d6d80ab41268bffb2adfe480c8d860ff
Niklas <nd@sql8r.net>
2020-05-22 16:33:14 +0200

    iffy

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -7,16 +7,33 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
+	UBullCowCartridge::HiddenWord = TEXT("isogram");
+
 	PrintLine(TEXT("Moh There! (Tab to focus)"));
-	PrintLine(TEXT("Please type text to play!"));
+	PrintLine(TEXT("Welcome to Bull Cows"));
+	PrintLine(TEXT("Please type a guess to play!"));
+	//PrintLine(HiddenWord.Len());
+	PrintLine(TEXT("And then press enter"));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
 	ClearScreen();
 
-	FString HiddenWord = TEXT("isogram");
 	// std::cout << HiddenWord;;
 
+	if(Input == HiddenWord)
+	{
+		PrintLine(TEXT("Correct"));
+		PrintLine(Input);
+
+	}
+	else
+	{
+		PrintLine(TEXT("Wröng"));
+		PrintLine(Input);
+
+//		Lifes--
+	}
 	
 }
