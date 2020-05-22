8e24ebb9efac965b2c3f1f6ea0dabf7306963e0f
Niklas <nd@sql8r.net>
2020-05-22 15:01:52 +0200

    TEXT

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,12 +1,22 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
+#include <iostream>
+
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+	PrintLine(TEXT("Moh There! (Tab to focus)"));
+	PrintLine(TEXT("Please type text to play!"));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+	ClearScreen();
 
+	FString HiddenWord = TEXT("isogram");
+	// std::cout << HiddenWord;;
+
+	
 }
