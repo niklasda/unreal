92d2ed6e919fb4dceac1d3a408f694bd79d00cb2
Niklas <nd@sql8r.net>
2020-05-25 14:53:10 +0200

    refac

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -5,35 +5,73 @@
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
-    Super::BeginPlay();
+	Super::BeginPlay();
 
-	UBullCowCartridge::HiddenWord = TEXT("isogram");
-
-	PrintLine(TEXT("Moh There! (Tab to focus)"));
-	PrintLine(TEXT("Welcome to Bull Cows"));
-	PrintLine(TEXT("Please type a guess to play!"));
-	//PrintLine(HiddenWord.Len());
-	PrintLine(TEXT("And then press enter"));
+	SetupGame();
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-	ClearScreen();
+	/*int32 a = 1;
+	PrintLine(TEXT("%i"), a);
+	*/
 
-	// std::cout << HiddenWord;;
-
-	if(Input == HiddenWord)
+	if (bGameOver)
 	{
-		PrintLine(TEXT("Correct"));
-		PrintLine(Input);
-
+		SetupGame();
+		
 	}
 	else
 	{
-		PrintLine(TEXT("Wröng"));
-		PrintLine(Input);
+		if (Input == HiddenWord)
+		{
+			PrintLine(TEXT("Correct"));
+			EndGame();
+		}
+		else if (HiddenWord.Len() == Input.Len())
+		{
+			Lives--;
 
-//		Lifes--
+			PrintLine(TEXT("Correct Length, Wrong word, %i lives remaingin"), Lives);
+
+		}
+		else //if (HiddenWord.Len() != Input.Len())
+		{
+			Lives--;
+			PrintLine(TEXT("Wrong Word, %i lives remaingin"), Lives);
+
+		}
+
+		if(Lives<= 0)
+		{
+			EndGame();
+		}
 	}
-	
+}
+
+void UBullCowCartridge::SetupGame()
+{
+	HiddenWord = TEXT("isogram");
+	Lives = HiddenWord.Len();
+	bGameOver = false;
+
+	ClearScreen();
+
+	PrintLine(TEXT("The Hidden word is: %s"), *HiddenWord);
+	PrintLine(TEXT("The word is %i characters long"), HiddenWord.Len());
+
+	PrintLine(TEXT("Moh There! (Tab to focus)"));
+	PrintLine(TEXT("Welcome to Bull Cows"));
+	PrintLine(TEXT("Please type a guess to play!"));
+
+	PrintLine(TEXT("And then press enter"));
+}
+
+void UBullCowCartridge::EndGame()
+{
+	//ClearScreen();
+
+	bGameOver = true;
+	PrintLine(TEXT("Game Over, press enter to play again"));
+
 }
